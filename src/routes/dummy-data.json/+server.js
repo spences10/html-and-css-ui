2dba90d882928eeff1403acb4ccf1b333f59f164
spences10 <spences10apps@gmail.com>
2022-12-03 08:42:40 +0000

    refactor: :recycle: moce data outside of GET call

--- src/routes/dummy-data.json/+server.js
+++ src/routes/dummy-data.json/+server.js
@@ -1,49 +1,49 @@
 /** @type {import('./$types').RequestHandler} */
 export function GET() {
-	const data = [
-		{
-			name: 'Lelah Nichols',
-			img: `https://joeschmoe.io/api/v1/jess`,
-			location: 'Troy, MI',
-			tags: [`clothes`, `stem`],
-			roles: [`new users`],
-		},
-		{
-			name: 'Jesus Weiss',
-			img: `https://joeschmoe.io/api/v1/jack`,
-			location: 'Fort Worth, TX',
-			tags: [`headset`, `gadget`, `speed`, `winter`],
-			roles: [`new users`],
-		},
-		{
-			name: 'Annie Rice',
-			img: `https://joeschmoe.io/api/v1/jenni`,
-			location: 'Austin, TX',
-			tags: [`road`, `mountain`, `trip`, `earth`, `nature`],
-			roles: [`new users`],
-		},
-		{
-			name: 'Robert Brower',
-			img: `https://joeschmoe.io/api/v1/jed`,
-			location: 'Cincinnati, OH',
-			tags: [`maintenance`, `gears`, `farmers`, `repair`],
-			roles: [`new users`],
-		},
-		{
-			name: 'Amy Campbell',
-			img: `https://joeschmoe.io/api/v1/jazebelle`,
-			location: 'Warrior, AL',
-			tags: [`music`, `discs`],
-			roles: [`new users`],
-		},
-		{
-			name: 'Anthony S. Morin',
-			img: `https://joeschmoe.io/api/v1/jana`,
-			location: 'Lyndhurst, NJ',
-			tags: [`vintage`, `electric`],
-			roles: [`new users`],
-		},
-	]
-
 	return new Response(JSON.stringify(data, null, 2))
 }
+
+const data = [
+	{
+		name: 'Lelah Nichols',
+		img: `https://joeschmoe.io/api/v1/jess`,
+		location: 'Troy, MI',
+		tags: [`clothes`, `stem`],
+		roles: [`new users`],
+	},
+	{
+		name: 'Jesus Weiss',
+		img: `https://joeschmoe.io/api/v1/jack`,
+		location: 'Fort Worth, TX',
+		tags: [`headset`, `gadget`, `speed`, `winter`],
+		roles: [`new users`],
+	},
+	{
+		name: 'Annie Rice',
+		img: `https://joeschmoe.io/api/v1/jenni`,
+		location: 'Austin, TX',
+		tags: [`road`, `mountain`, `trip`, `earth`, `nature`],
+		roles: [`new users`],
+	},
+	{
+		name: 'Robert Brower',
+		img: `https://joeschmoe.io/api/v1/jed`,
+		location: 'Cincinnati, OH',
+		tags: [`maintenance`, `gears`, `farmers`, `repair`],
+		roles: [`new users`],
+	},
+	{
+		name: 'Amy Campbell',
+		img: `https://joeschmoe.io/api/v1/jazebelle`,
+		location: 'Warrior, AL',
+		tags: [`music`, `discs`],
+		roles: [`new users`],
+	},
+	{
+		name: 'Anthony S. Morin',
+		img: `https://joeschmoe.io/api/v1/jana`,
+		location: 'Lyndhurst, NJ',
+		tags: [`vintage`, `electric`],
+		roles: [`new users`],
+	},
+]
